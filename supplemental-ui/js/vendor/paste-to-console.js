8103d4e28a4ac8bb95a1a957b34db896b50ba88a
Alberto Gonzalez Rodriguez <josegonz@redhat.com>
2024-02-13 13:34:03 +0100

    Create paste-to-console.js

---
+++ supplemental-ui/js/vendor/paste-to-console.js
@@ -0,0 +1,5 @@
+var copybuttons = document.getElementsByClassName("copy-button")
+function paste() {
+  window.parent.document.getElementById('terminal_01').contentWindow.document.execCommand('insertText', false, 'ls');
+}
+Array.prototype.forEach.call(copybuttons, function (b) { addEventListener('click', paste); });
