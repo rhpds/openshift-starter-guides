e4efd141ff25f349988ffe3c858c88c009dbe2ef
Alberto Gonzalez <josegonz@redhat.com>
2024-02-13 14:34:56 +0100

    add paste functionality

--- supplemental-ui/js/vendor/paste-to-console.js
+++ supplemental-ui/js/vendor/paste-to-console.js
@@ -1,8 +1,8 @@
 function addPasteFunctionality () {
   var copybuttons = document.getElementsByClassName("copy-button")
   function paste(el) {
-    console.log(el)
-    window.parent.document.getElementById('terminal_01').contentWindow.document.execCommand('insertText', false, el.nextSibling.innerText);
+    console.log(el.target)
+    window.parent.document.getElementById('terminal_01').contentWindow.document.execCommand('insertText', false, el.target.nextSibling.innerText);
   }
   Array.prototype.forEach.call(copybuttons, function (b) { console.log(b); b.addEventListener('click', paste); });
 }
