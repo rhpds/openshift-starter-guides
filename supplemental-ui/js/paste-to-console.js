5346850694b2789bfcac9312c78630258409d8ac
Alberto Gonzalez Rodriguez <josegonz@redhat.com>
2024-02-13 13:33:46 +0100

    Delete supplemental-ui/js/paste-to-console.js

--- supplemental-ui/js/paste-to-console.js
+++
@@ -1,5 +0,0 @@
-var copybuttons = document.getElementsByClassName("copy-button")
-function paste() {
-  window.parent.document.getElementById('terminal_01').contentWindow.document.execCommand('insertText', false, 'ls');
-}
-Array.prototype.forEach.call(copybuttons, function (b) { addEventListener('click', paste); });
